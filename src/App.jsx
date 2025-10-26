b08d32974c39db93dbf412bb8efc1d02f4087dd3
Naman Singla <namansingla830@gmail.com>
2025-10-26 13:14:23 +0530

    fix tailwind problem in padding

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <>
-    <div className="min-h-screen bg-gray-100 ">
+    <div className="min-h-screen bg-gray-100 p-4">
       <h1 className="text-center text-4xl font-bold">
         Hello Naman your solution of vitereact is working well
       </h1>
